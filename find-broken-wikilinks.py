a92d464c3f7fb2f9dd76bd15a7b0ead793a69440
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2022-01-05 03:50:52 +0100

    rewrite the comment detection system to be less naive
    `is_in_comment()` was previously not able to take into account
    multiple comments on a single line or multiline comments.
    
    the approach taken for this was chosen in order to preserve
    line-by-line parsing.

--- find-broken-wikilinks.py
+++ find-broken-wikilinks.py
@@ -3,7 +3,40 @@ import os
 import sys
 import typing
 
+
 Redirects = typing.Dict[str, typing.Tuple[str, int]]
+
+
+class Comment(typing.NamedTuple):
+    """
+    An HTML comment in a line.
+
+    These mark regions where parsed content should be discarded.
+
+    Example:
+        <!-- this is a comment -->
+        ^ start                  ^ end
+
+    Since an article is read and errors are printed line-by-line
+    (to protect against unexpected crashes), multiline comments are
+    expressed by setting the start and/or end values to -1, indicating
+    continuation of a comment from a previous line or to subsequent
+    lines respectively.
+
+    Examples:
+
+        A multiline comment continuing from a previous line -->
+        (start = -1)                                          ^ end
+
+        <!-- A multiline comment continuing off to subsequent lines
+        ^ start                                          (end = -1)
+
+        A whole line marked as part of a multiline comment
+        (start = -1)                          (end = -1)
+    """
+
+    start: int
+    end: int
 
 class Link(typing.NamedTuple):
     """
@@ -128,8 +161,55 @@ def check_link(redirects: Redirects, directory: str, link: str) -> typing.Tuple[
         return (True, "")
 
 
-def is_in_comment(s, start, end):
-    return s.rfind("<!--", 0, start) != -1 and s.find("-->", end, -1)
+def find_comments(line: str, in_multiline: bool) -> typing.List[Comment]:
+    comments = []
+    index = 0
+    start = None
+
+    while True:
+        # don't start a comment if already in one
+        if not in_multiline:
+            start = line.find("<!--", index)
+            if start == -1:
+                # no more comments
+                return comments
+
+        end = line.find("-->", start or 0)
+
+        if end != -1:
+            # found the end of a comment
+            if in_multiline:
+                # end of a multiline comment
+                comments.append(Comment(start=-1, end=end + 2))
+                in_multiline = False
+            else:
+                # whole inline comment
+                comments.append(Comment(start=start, end=end + 2))
+            index = end + 3
+            continue
+        elif start is None:
+            # no comment start or end; the whole line is part of a comment
+            comments.append(Comment(start=-1, end=-1))
+            return comments
+        else:
+            # unmatched comment start: continuing to subsequent lines
+            comments.append(Comment(start=start, end=-1))
+            return comments
+
+
+def is_in_comment(index: int, comments: typing.List[Comment]) -> bool:
+    for comment in comments:
+        left_bound = comment.start
+        right_bound = comment.end
+        if comment.start == -1:
+            left_bound = float('-inf')
+        if comment.end == -1:
+            right_bound = float('inf')
+
+        if index >= left_bound and index <= right_bound:
+            return True
+
+    return False
 
 
 def find_link(s: str, index=0) -> typing.Optional[Link]:
@@ -167,8 +247,6 @@ def find_link(s: str, index=0) -> typing.Optional[Link]:
             parenthesis_level -= 1
             if parenthesis_level == 0:
                 end = i
-                if is_in_comment(s, start, end):
-                    return None
                 if extra is None:
                     extra = end
 
@@ -252,8 +330,16 @@ def main():
             continue
 
         with open(filename, 'r', encoding='utf-8') as fd:
+            in_multiline = False
             for linenumber, line in enumerate(fd, start=1):
+                comments = find_comments(line, in_multiline)
+                if comments:
+                    in_multiline = comments[-1].end == -1
+
                 for match in find_links(line):
+                    if is_in_comment(match.link_start, comments):
+                        continue
+
                     if match.content == "/wiki/Sitemap":
                         continue
                     success, note = check_link(redirects, directory(filename), match.location)
@@ -263,6 +349,7 @@ def main():
                     if exit_code == 0:
                         print_error()
                     exit_code = 1
+
                     print(f"{yellow(filename)}:{linenumber}:{match.link_start + 1}: {red(match.location)}")
                     if note:
                         print(note)
