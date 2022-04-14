Walavouchey/osu-wiki-tools

commit f02009be5bd55d7366b3c15ea990b8402b85d860

initialize variables according to typing hints

wikitools/comment_parser.py
@@ -34,7 +34,7 @@ class Comment(typing.NamedTuple):
 def parse(line: str, in_multiline: bool = False) -> typing.List[Comment]:
     comments: typing.List[Comment] = []
     index = 0
-    start = None
+    start = 0
 
     while True:
         # don't start a comment if already in one
@@ -44,7 +44,7 @@ def parse(line: str, in_multiline: bool = False) -> typing.List[Comment]:
                 # no more comments
                 return comments
 
-        end = line.find("-->", start or 0)
+        end = line.find("-->", start)
 
         if end != -1:
             # found the end of a comment
@@ -72,9 +72,9 @@ def is_in_comment(index: int, comments: typing.List[Comment]) -> bool:
         left_bound = comment.start
         right_bound = comment.end
         if comment.start == -1:
-            left_bound = float('-inf')
+            left_bound = float('-inf')  # type: ignore
         if comment.end == -1:
-            right_bound = float('inf')
+            right_bound = float('inf')  # type: ignore
 
         if index >= left_bound and index <= right_bound:
             return True

wikitools/link_parser.py
@@ -136,8 +136,8 @@ def find_link(s: str, index=0) -> typing.Optional[Link]:
 
     state = State.IDLE
 
-    start = None
-    location = None
+    start = -1
+    location = -1
     extra = None
     end = None
 

commit 891341d7b92f858aaa7dcd39be01379761842b99

correctly determine the end of a multiline comment
this was broken in f02009be5bd55d7366b3c15ea990b8402b85d860 --
every multiline comment has got an extra line accounted towards it

wikitools/comment_parser.py
@@ -34,7 +34,7 @@ class Comment(typing.NamedTuple):
 def parse(line: str, in_multiline: bool = False) -> typing.List[Comment]:
     comments: typing.List[Comment] = []
     index = 0
-    start = 0
+    start = -1
 
     while True:
         # don't start a comment if already in one
@@ -44,7 +44,7 @@ def parse(line: str, in_multiline: bool = False) -> typing.List[Comment]:
                 # no more comments
                 return comments
 
-        end = line.find("-->", start)
+        end = line.find("-->", 0 if start == -1 else start)
 
         if end != -1:
             # found the end of a comment
@@ -57,7 +57,7 @@ def parse(line: str, in_multiline: bool = False) -> typing.List[Comment]:
                 comments.append(Comment(start=start, end=end + 2))
             index = end + 3
             continue
-        elif start is None:
+        elif start == -1:
             # no comment start or end; the whole line is part of a comment
             comments.append(Comment(start=-1, end=-1))
             return comments

tests/test_article_parser.py
@@ -3,7 +3,7 @@ from urllib import parse
 
 import conftest
 
-from wikitools import article_parser, reference_parser
+from wikitools import article_parser, reference_parser, link_parser
 
 
 class TestArticleParser:
@@ -117,3 +117,30 @@ class TestArticleParser:
             'random.1',
             'section.2',
         }
+
+    def test__ignore_comments(self, root):
+        conftest.create_files(
+            root,
+            (
+                'Comments/en.md',
+                textwrap.dedent('''
+                    # Comments
+
+                    <!-- Don't mention [comments](/wiki/HTML#comment). -->
+
+                    <!-- Don't mention the [comments](/wiki/HTML#comment) at all.
+                        Yes, even if they span across several [lines](/wiki/Power_line).
+                        Please be [silent](/wiki/Silence) about that, okay? --> [Test](/wiki/Test)
+
+                    There is [no](/wiki/No) support<!-- for the [comments](/wiki/HTML#comment) --> on the wiki.
+                ''').strip()
+            )
+        )
+        article = article_parser.parse('wiki/Comments/en.md')
+        assert set(article.lines.keys()) == {7, 9}
+
+        assert len(article.lines[7].links) == 1
+        assert article.lines[7].links[0].raw_location == "/wiki/Test"
+
+        assert len(article.lines[9].links) == 1
+        assert article.lines[9].links[0].raw_location == "/wiki/No"

tests/test_comment_parser.py
@@ -1,0 +1,50 @@
+from wikitools import comment_parser
+
+
+class TestCommentParser:
+    def test__inline_comments(self):
+        for line, expected in (
+            ("Empty", []),
+            (
+                "<!-- A single comment-->",
+                [comment_parser.Comment(start=0, end=23)]
+            ),
+            (
+                "<!-- Several --><!-- comments -->",
+                [
+                    comment_parser.Comment(start=0, end=15),
+                    comment_parser.Comment(start=16, end=32),
+                ]
+            ),
+        ):
+            assert comment_parser.parse(line) == expected
+
+    def test__multiline_comments(self):
+        lines = [
+            "I know my <!-- A",
+            "B",
+            "C",
+            "and other --> letters.<!-- test -->",
+            "Take a break",
+            "<!-- and",
+            "continue -->",
+        ]
+
+        comments = []
+        # TODO: this is copied from article_parser.parse() while it should be a standalone piece of code
+        in_multiline = False
+        for line in lines:
+            new_comments = comment_parser.parse(line, in_multiline)
+            if new_comments:
+                in_multiline = new_comments[-1].end == -1
+            comments.extend(new_comments)
+
+        assert comments == [
+            comment_parser.Comment(start=10, end=-1),
+            comment_parser.Comment(start=-1, end=-1),
+            comment_parser.Comment(start=-1, end=-1),
+            comment_parser.Comment(start=-1, end=12),
+            comment_parser.Comment(start=22, end=34),
+            comment_parser.Comment(start=0, end=-1),
+            comment_parser.Comment(start=-1, end=11),
+        ]