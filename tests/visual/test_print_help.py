c407f532da6e1e012e296058c43a0768dba5f0d6
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2023-01-06 11:25:52 +0100

    add visual test case for the check-yaml command

--- tests/visual/test_print_help.py
+++ tests/visual/test_print_help.py
@@ -1,7 +1,7 @@
 from tests.conftest import VisualTest, VisualTestCase
 
 from wikitools_cli import osu_wiki_tools
-from wikitools_cli.commands import check_links, check_outdated_articles
+from wikitools_cli.commands import check_links, check_outdated_articles, check_yaml
 
 test = VisualTest(
     name="Print help",
@@ -18,6 +18,10 @@ test = VisualTest(
         VisualTestCase(
             description="check_outdated_articles.main run with --help",
             function=lambda : check_outdated_articles.main("--help")
+        ),
+        VisualTestCase(
+            description="check_yaml.main run with --help",
+            function=lambda : check_yaml.main("--help")
         ),
     ]
 )
