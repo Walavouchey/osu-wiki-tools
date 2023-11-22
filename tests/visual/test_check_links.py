89d277e073586c57f124e9e4dba12afb6da32616
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2023-11-22 23:19:00 +0100

    fix broken visual test

--- tests/visual/test_check_links.py
+++ tests/visual/test_check_links.py
@@ -30,7 +30,7 @@ test = VisualTest(
         VisualTestCase(
             name="missing_identifier",
             description="Missing identifier (1 error)",
-            function=lambda : check_links.main("--root", "tests/test_articles", "--outdated",  "--target", "wiki/missing_identifier/en.md")
+            function=lambda : check_links.main("--root", "tests/test_articles",  "--target", "wiki/missing_identifier/en.md")
         ),
     ]
 )
