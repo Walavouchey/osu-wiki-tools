cf9d2a74b6f02b556b8cac8b5b8fa1c3d1bef713
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2023-11-23 16:58:56 +0100

    fix redirect parser

--- wikitools/redirect_parser.py
+++ wikitools/redirect_parser.py
@@ -3,6 +3,13 @@ import typing
 Redirects = typing.Dict[str, typing.Tuple[str, int]]
 
 
+def unquote_and_trim(s):
+    s = s.strip()
+    if (s[0] == '"' and s[-1] == '"') or (s[0] == "'" and s[-1] == "'"):
+        return s[1:-1]
+    return s
+
+
 def load_redirects(path: str) -> Redirects:
     """
     Read redirects from a YAML dictionary file (done manually to preserve line numbers).
@@ -11,9 +18,9 @@ def load_redirects(path: str) -> Redirects:
     redirects = {}
     with open(path, 'r', encoding='utf-8') as fd:
         for line_number, line in enumerate(fd, start=1):
-            split = line.split('"')
+            split = line.split(':')
             try:
-                redirects[split[1]] = (split[3], line_number)
+                redirects[unquote_and_trim(split[0])] = (unquote_and_trim(split[1]), line_number)
             except IndexError:
                 pass
     return redirects
