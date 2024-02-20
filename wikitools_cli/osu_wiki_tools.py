b8cf8c73be49180f736dd49b3d98e093b7d10e7c
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2024-02-20 21:11:40 +0100

    add update_originals command

--- wikitools_cli/osu_wiki_tools.py
+++ wikitools_cli/osu_wiki_tools.py
@@ -7,7 +7,7 @@ Tools useful for osu! wiki contributors
 import argparse
 import sys
 
-from wikitools_cli.commands import check_outdated_articles, check_links, check_yaml
+from wikitools_cli.commands import check_outdated_articles, check_links, check_yaml, update_originals
 
 from wikitools_cli.VERSION import VERSION
 
@@ -26,6 +26,11 @@ commands = [
         "name": "check-yaml",
         "help": "validate front matter and standalone YAML files",
         "entry": check_yaml.main,
+    },
+    {
+        "name": "update-originals",
+        "help": "update the \"wiki/osu! originals\" article",
+        "entry": update_originals.main,
     },
 ]
 
